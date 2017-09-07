c6bd176b02a3f2d0c25bfb4d2a4b39c8f90cc7b4
Martin-Kotrulev <martin.kotrulev@gmail.com>
2017-09-08 00:58:06 +0300

    added migration runtime

--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Persistence;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace App
@@ -14,12 +17,32 @@ namespace App
   {
     public static void Main(string[] args)
     {
-      BuildWebHost(args).Run();
+      BuildWebHost(args)
+        .MigrateDatabase()
+        .Run();
+
+      
     }
 
     public static IWebHost BuildWebHost(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
             .UseStartup<Startup>()
             .Build();
+  }
+
+  public static class MigrationConfig
+  {
+    public static IWebHost MigrateDatabase(this IWebHost webHost)
+    {
+        using (var scope = webHost.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var dbContext = services.GetRequiredService<AppDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+
+        return webHost;
+    }
   }
 }
